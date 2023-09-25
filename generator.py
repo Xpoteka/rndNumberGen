a30e55fa42a713257ed8b3794d49baa34f527d17
Hamel Jérémie <jeremie.hamel@hpbsc.ch>
2023-09-25 10:19:35 +0200

    genEinschränken eingeführt

--- generator.py
+++ generator.py
@@ -1,7 +1,24 @@
 import random
 
-def main(von, bis):
+def genNormal(von, bis):
     print(random.randint(von, bis))
 
 
-main(1, 100)
+def genEinschraenkend():
+    vonbis = []
+    einschraenkungen = []
+    vonbis = input("Geben Sie die erste Zahl ein:")
+    vonbis = input("Geben Sie die zweite Zahl ein:")
+    einschränken = input("Wollen Sie Ihre Eingabe einschränken? yes? no?:")
+    match einschränken:
+        case "yes":
+            while einschränken != "no":
+                einschraenkungen = input("Geben Sie ihre Einschränkung ein:")
+                einschränken = input("Wollen Sie Ihre Eingabe weiter einschränken? yes? no?:")
+        case _:
+            print("keine Einschränkung")
+    
+    print(random.choice([i for i in vonbis if i not in einschraenkungen]))
+
+
+genEinschraenkend()
